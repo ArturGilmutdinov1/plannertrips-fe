07b069144082fe987856b43fdb3d759e33d695d8
ArturGilmutdinov1 <Artur-08.09.1999@Yandex.ru>
2026-08-20 20:50:59 +0500

    feat: add button

---
+++ app/components/Button.vue
@@ -0,0 +1,25 @@
+<template>
+    <UButton @click="handleClick">{{ label }}</UButton>
+</template>
+
+<script setup>
+    const props = defineProps({
+        label: {
+            type: String,
+            required: true
+        },
+        href: {
+            type: String,
+            default: null
+        }
+    })
+
+    const emit = defineEmits(['click'])
+     const handleClick = () => {
+        emit('click')
+
+        if(props.href){
+            navigateTo(props.href)
+        }
+    }
+</script>
